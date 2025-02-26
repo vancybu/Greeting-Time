faaae2dc5237548e7e8bee9ec049a07067f69ba0
Vancy bukama <vancybukama@gmail.com>
2025-02-26 09:13:22 +0200

    Changed Copyright

--- index.js
+++ index.js
@@ -2,24 +2,25 @@
 const currentDate = new Date();
 
 // 2. Get the current hour
-let getHours = currentDate.getHours();
+const getHours = currentDate.getHours();
 
 // 3. Get the current minutes
-let getMinutes = currentDate.getMinutes();
+const getMinutes = currentDate.getMinutes().toString().padStart(2, '0');
 
 // 4. Get the current seconds
-let getSeconds = currentDate.getSeconds();
+const getSeconds = currentDate.getSeconds().toString().padStart(2, '0');
 
 // 5. Get the current year
-let getFullYear = currentDate.getFullYear();
+const getFullYear = currentDate.getFullYear();
 
-// 5. Create a variable to store the time
-let greeting = getHours + ":" + getMinutes + ":" + getSeconds;
+// 6. Create a variable to store the time
+let time = `${getHours}:${getMinutes}:${getSeconds}`;
 
-//Log the time to the console
-console.log(greeting);
+// Log the time to the console
+console.log(time);
 
-// 6. Determine the appropriate greeting based on the current hour
+// 7. Determine the appropriate greeting based on the current hour
+let greeting;
 if (getHours < 12) {
     greeting = "Good Morning";
 } else if (getHours < 18) {
@@ -28,31 +29,31 @@ if (getHours < 12) {
     greeting = "Good Evening";
 }
 
-// 10. Select the h1 element
+// 8. Select the h1 element
 const heading = document.querySelector("h1");
 
-// 11. Change the innerHTML of the h1 element to display the greeting
+// 9. Change the innerHTML of the h1 element to display the greeting
 heading.innerHTML = greeting;
 
-// 7. Create a variable to store the image URL
+// 10. Create a variable to store the image URL
 const img = "https://picsum.photos/200";
 
-// 8. Select the image element
+// 11. Select the image element
 const image = document.querySelector("img");
 
-// 9. Change the src attribute of the image element
+// 12. Change the src attribute of the image element
 image.src = img;
 
-// 12. Select the p element with the class of copyright
+// 13. Select the p element with the class of copyright
 const copyright = document.querySelector(".copyright");
 
-// 13. Create a new span element
+// 14. Create a new span element
 const year = document.createElement("span");
 
-// 14. Set the innerHTML of the span element to the current year
+// 15. Set the innerHTML of the span element to the current year
 year.innerHTML = getFullYear;
 
-// 15. Append the span element to the p element
+// 16. Append the span element to the p element
 copyright.appendChild(year);
 
 
